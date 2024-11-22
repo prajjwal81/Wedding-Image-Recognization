629f6a6ee9a68d7c6dae76fe38b3a5952503862d
Anand Stephan <anandstephan98@gmail.com>
2024-11-22 17:50:37 +0530

    feat: completed the screen

--- src/Navigations/OTP.tsx
+++ src/Navigations/OTP.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 
 const OTP = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>Enter Your OTP</Text>
-      <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text style={styles.para}>Lorem ipsum dolor sit amet.</Text>
+      <View style={styles.rowContainer}>
+        <TextInput style={styles.inputContainer} />
+        <TextInput style={styles.inputContainer} />
+        <TextInput style={styles.inputContainer} />
+        <TextInput style={styles.inputContainer} />
+      </View>
+      <Pressable style={styles.btnContainer}>
+        <Text style={styles.btnTxt}>Continue</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
@@ -13,14 +23,47 @@ export default OTP;
 
 const styles = StyleSheet.create({
   container: {
-    margin: 10,
-    padding: 10,
+    // padding: 10,
     // flex: 1,
-    borderWidth: 10,
+
+    marginHorizontal: 20,
+    marginTop: 120,
   },
   heading: {
     fontFamily: 'Roboto-Regular',
     fontSize: 24,
     fontWeight: '600',
+    // marginVertical: 10,
+  },
+  para: {
+    fontFamily: 'Inter_18pt-BoldItalic',
+    fontWeight: '400',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    width: 64,
+    height: 64,
+    borderRadius: 10,
+    borderColor: '#8E8E93',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  btnContainer: {
+    backgroundColor: '#00897B',
+    padding: 18,
+    marginTop: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+  },
+  btnTxt: {
+    color: '#FFFFFF',
   },
 });
