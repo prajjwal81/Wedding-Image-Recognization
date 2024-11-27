d32c678dade27d7c658769f89cbdb3a5872644cd
Anand Stephan <anandstephan98@gmail.com>
2024-11-27 10:04:22 +0530

    feat:adding modal

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,15 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  SafeAreaView,
+  Pressable,
+} from 'react-native';
+import React, {useState} from 'react';
 import YourAlbum from './components/YourAlbum';
 import Photos from '../common/Photos';
 
 export default function Home() {
+  const [modal, setModal] = useState(true);
   return (
-    <View>
+    <SafeAreaView>
       {/* <YourAlbum /> */}
       <Photos />
-    </View>
+      <View style={styles.container}>
+        <Modal
+          visible={modal}
+          animationType="slide"
+          transparent
+          style={{borderWidth: 2}}>
+          <Pressable
+            style={styles.fakeHeight}
+            onPress={() => {
+              setModal(false);
+            }}
+          />
+          <Text>Select All Your Photos</Text>
+          <Pressable>
+            <Text>All Photos</Text>
+          </Pressable>
+          <Pressable>
+            <Text>Your Photos</Text>
+          </Pressable>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    // borderWidth: 1,
+  },
+  heading: {
+    borderWidth: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+  },
+  fakeHeight: {
+    height: '70%',
+    backgroundColor: 'rgba(60, 60, 60, 0.5)',
+  },
+});
